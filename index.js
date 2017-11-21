876a67b7c571ffea19e78e1bff877a02072e9f2a
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-21 19:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function nestedTarget() {
   return document.getElementById("nested").children[0];
 }
 
+function increaseRankBy(n) {
+  
+}
+
 function deepestChild() {
   var children = document.getElementById("grand-node").querySelectorAll("div");
   return children[3];
