42e7dfe6829384aa87f12ee8a662c305ac486707
Da Woon Lee <daniel.lee9415@gmail.com>
2017-11-21 19:48:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var list = document.getElementsByClassName("ranked-list");
+  var list = document.getElementsByClassName("ranked-list").children;
   for (var i = 0; i < list.length; i++) {
     parseInt(list[i]);
     list[i]++;
